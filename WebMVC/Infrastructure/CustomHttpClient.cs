84bb1045ef27ed598462906a79f8774cbe99814d
Kal <info@kalacademy.org>
2025-02-09 12:18:39 -0800

    Dockerize and started webMVC

---
+++ WebMVC/Infrastructure/CustomHttpClient.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http.Headers;
+namespace WebMVC.Infrastructure
+{
+    public class CustomHttpClient : IHttpClient
+    {
+        private readonly HttpClient _httpClient;
+        public CustomHttpClient() 
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public Task<HttpResponseMessage> DeleteAsync(string uri, string authorizationToken = null, string authorizationMethod = "Bearer")
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetStringAsync(string uri, 
+            string authorizationToken = null, string authorizationMethod = "Bearer")
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
+            if (authorizationToken != null)
+            {
+                requestMessage.Headers.Authorization = new 
+                    AuthenticationHeaderValue(authorizationMethod, 
+                    authorizationToken);
+            }
+            var response = await _httpClient.SendAsync(requestMessage);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public Task<HttpResponseMessage> PostAsync<T>(string uri, T item, string authorizationToken = null, string authorizationMethod = "Bearer")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> PutAsync<T>(string uri, T item, string authorizationToken = null, string authorizationMethod = "Bearer")
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebMVC/Services/ICatalogService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using WebMVC.Models;
+
+namespace WebMVC.Services
+{
+    public interface ICatalogService
+    {
+        Task<IEnumerable<SelectListItem>> GetBrandsAsync();
+        Task<IEnumerable<SelectListItem>> GetTypesAsync();
+        Task<Catalog> GetCatalogItemsAsync(int page, int size, int? brand, int? type);
+    }
+}
