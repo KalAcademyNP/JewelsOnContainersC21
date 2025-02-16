ba2c80eca089e67c90d4a354428e45e39b7f0516
Kal <info@kalacademy.org>
2025-02-16 12:15:28 -0800

    AuthAPI

---
+++ AuthAPI/Program.cs
@@ -0,0 +1,40 @@
+using AuthAPI.Data;
+using AuthAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(configuration["ConnectionString"]);
+});
+builder.Services.Configure<JwtOptions>(configuration.GetSection("JWT"));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ AuthAPI/Services/AuthService.cs
@@ -0,0 +1,91 @@
+﻿using AuthAPI.Data;
+using AuthAPI.Models;
+using AuthAPI.Models.Dto;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthAPI.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly AppDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+        public AuthService(AppDbContext db, 
+            IJwtTokenGenerator jwtTokenGenerator, 
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }
+
+        public Task<bool> AssignRole(string email, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ResponseDto> Register(RegistrationRequestDto 
+            registrationRequestDto)
+        {
+            ApplicationUser user = new ApplicationUser()
+            {
+                UserName = registrationRequestDto.Email,
+                Email = registrationRequestDto.Email,
+                NormalizedEmail = registrationRequestDto.Email.ToUpper(),
+                Name = registrationRequestDto.Name,
+                PhoneNumber = registrationRequestDto.PhoneNumber
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(user,
+                    registrationRequestDto.Password);
+                if (result.Succeeded)
+                {
+                    var userToReturn = _db.ApplicationUsers.First(u =>
+                        u.UserName == registrationRequestDto.Email);
+
+                    var userDto = new UserDto()
+                    {
+                        Email = userToReturn.Email,
+                        ID = userToReturn.Id,
+                        Name = userToReturn.Name,
+                        PhoneNumber = userToReturn.PhoneNumber
+                    };
+
+                    return new ResponseDto()
+                    {
+                        Result = userDto,
+                        IsSuccess = true,
+                        Message = string.Empty
+                    };
+                } else
+                {
+                    return new ResponseDto()
+                    {
+                        Result = null,
+                        IsSuccess = false,
+                        Message = result.Errors.FirstOrDefault().Description
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ResponseDto()
+                {
+                    Result = null,
+                    IsSuccess = false,
+                    Message = ex.Message
+                };
+            }
+        }
+    }
+}
