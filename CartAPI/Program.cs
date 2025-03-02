feabc227eabd551766b97a8d3da3c06171f870a1
Kal <info@kalacademy.org>
2025-03-02 12:17:43 -0800

    Completed cart api and integration with webmvc

--- CartAPI/Program.cs
+++ CartAPI/Program.cs
@@ -1,11 +1,79 @@
+using CartAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using StackExchange.Redis;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddTransient<ICartRepository, RedisCartRepository>();
+builder.Services.AddSingleton<ConnectionMultiplexer>(cm =>
+{
+    var config = ConfigurationOptions.Parse(configuration["ConnectionString"], true);
+    config.ResolveDns = true;
+    config.AbortOnConnectFail = true;
+    return ConnectionMultiplexer.Connect(config);
+});
+
+var settingsSection = configuration.GetSection("JWT");
+var secret = settingsSection.GetValue<string>("Secret");
+var issuer = settingsSection.GetValue<string>("Issuer");
+var audience = settingsSection.GetValue<string>("Audience");
+
+var key = Encoding.ASCII.GetBytes(secret);
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidIssuer = issuer,
+        ValidateAudience = true,
+        ValidAudience = audience
+    };
+});
+builder.Services.AddAuthorization();
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option => {
+    option.AddSecurityDefinition(name: JwtBearerDefaults.AuthenticationScheme,
+        securityScheme: new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Description = "Enter the Bearer Authorization string as follows: 'Bearer Generated-JWT-Token'",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
+        });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id=JwtBearerDefaults.AuthenticationScheme
+                }
+            }, new String[]{ }
+        }
+    });
+
+});
 
 var app = builder.Build();
 
@@ -17,7 +85,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ WebMVC/Services/CartService.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using WebMVC.Infrastructure;
+using WebMVC.Models;
+using WebMVC.Models.CartModels;
+
+namespace WebMVC.Services
+{
+    public class CartService : ICartService
+    {
+        private readonly string _baseUrl;
+        private readonly IConfiguration _config;
+        private readonly IHttpClient _apiClient;
+        private readonly ITokenProvider _tokenProvider;
+        public CartService(IConfiguration config, IHttpClient client,
+            ITokenProvider tokenProvider)
+        {
+            _config = config;
+            _apiClient = client;
+            _tokenProvider = tokenProvider;
+            _baseUrl = $"{config["CartUrl"]}/api/cart";
+        }
+        private string GetUserToken()
+        {
+            return _tokenProvider.GetToken();
+        }
+        public async Task AddItemToCart(ApplicationUser user, CartItem product)
+        {
+            //Get the users cart
+            var cart = await GetCart(user);
+            //if the item exists in the cart. if yes, increase quantity. if no, add item
+            var basketItem = cart.Items.Where(p => p.ProductId == product.ProductId).FirstOrDefault();
+            if (basketItem == null)
+            {
+                cart.Items.Add(product);
+            }
+            else
+            {
+                basketItem.Quantity++;
+            }
+            await UpdateCart(cart);
+        }
+
+        public async Task ClearCart(ApplicationUser user)
+        {
+            var token = GetUserToken();
+            var cleanBasketUri = APIPaths.Basket.CleanBasket(_baseUrl, user.Email);
+            await _apiClient.DeleteAsync(cleanBasketUri, token);
+        }
+
+        public async Task<Cart> GetCart(ApplicationUser user)
+        {
+            var token = GetUserToken();
+            var getBasketUri = APIPaths.Basket.GetBasket(_baseUrl, user.Email);
+            var dataString = await _apiClient.GetStringAsync(getBasketUri, token);
+            var response = JsonConvert.DeserializeObject<Cart>(dataString) ??
+                new Cart
+                {
+                    BuyerId = user.Email
+                };
+            return response;
+        }
+
+        public async Task<Cart> SetQuantities(ApplicationUser user, 
+            Dictionary<string, int> quantities)
+        {
+            //Get cart
+            var basket = await GetCart(user);
+            basket.Items.ForEach(x =>
+            {
+                if (quantities.TryGetValue(x.Id, out var quantity))
+                {
+                    x.Quantity = quantity;
+                }
+            });
+            return basket;
+        }
+
+        public async Task<Cart> UpdateCart(Cart cart)
+        {
+            var token = GetUserToken();
+            var updateBasketUri = APIPaths.Basket.UpdateBasket(_baseUrl);
+            var response = await _apiClient.PostAsync(updateBasketUri, cart, token);
+
+            response.EnsureSuccessStatusCode();
+
+            return cart;
+        }
+    }
+}
